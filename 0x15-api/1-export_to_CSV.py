cc3812b0558e5638ae689bbaf21bdd2167799945
ThengweTheFirst <sithebemzwandie40@gmail.com>
2023-04-25 21:37:49 +0200

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 """
-Accessing a REST API for todo lists of employees
+Script to export data in the CSV format
 """
-import cvs
-import requests
+import csv
+import requests-
 import sys
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    usr = requests.get(url + "users/{}".format(user_id)).json()
+    username = usr.get("username")
+    to_do = requests.get(url + "todos", params={"userId": user_id}).json()
 
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-         ) for t in todos]
+        [writer.writerow([user_id, username, elm.get("completed"),
+                          elm.get("title")]) for elm in to_do]
